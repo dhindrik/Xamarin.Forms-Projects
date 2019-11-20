beabe26aad0f4b3cf27306e2c2f76764b54124c2
Johan Karlsson <johan.kson@gmail.com>
2019-11-20 21:43:21 +0100

    Readded settings.cs

---
+++ Chapter03/News/News/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace News
+{
+    public class Settings
+    {
+        public string NewsApiKey
+        {
+            get
+            { 
+                 return "<<register on newsapi.org to get your API key>>";
+            }
+        }
+    }
+}
